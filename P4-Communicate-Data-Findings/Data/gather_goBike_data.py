b0432eb6877a9b7956adc631979c7bd598b8ca1e
Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>
2019-03-16 13:17:44 +0100

    removed year_list slicing and added comments

--- P4-Communicate-Data-Findings/Data/gather_goBike_data.py
+++ P4-Communicate-Data-Findings/Data/gather_goBike_data.py
@@ -1,12 +1,17 @@
 import requests
 
+#define years to gather
 year_data = [x for x in range(201801, 201813)] + [x for x in range(201901, 201903)]
 
-for year in year_data[:3]:
-
+#loop over all files
+for year in year_data:
+    
+    #set the url dependend on the url
     url = f"https://s3.amazonaws.com/fordgobike-data/{year}-fordgobike-tripdata.csv.zip"
-        
+    
+    #request the url
     response = requests.get(url)
    
+    #open a file with the same name as the downloaded one and write to content to the file
     with open(f"{year}-fordgobike-tripdata.csv.zip", mode = "wb") as file:
         file.write(response.content)

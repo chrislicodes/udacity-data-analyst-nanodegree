d1dd8799fb3b27293a5d5cfaf9b2aeade0da0aa5
Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>
2019-03-16 13:16:11 +0100

    added code to gather_goBike_data.py

--- P4-Communicate-Data-Findings/Data/gather_goBike_data.py
+++ P4-Communicate-Data-Findings/Data/gather_goBike_data.py
@@ -1,1 +1,12 @@
+import requests
 
+year_data = [x for x in range(201801, 201813)] + [x for x in range(201901, 201903)]
+
+for year in year_data[:3]:
+
+    url = f"https://s3.amazonaws.com/fordgobike-data/{year}-fordgobike-tripdata.csv.zip"
+        
+    response = requests.get(url)
+   
+    with open(f"{year}-fordgobike-tripdata.csv.zip", mode = "wb") as file:
+        file.write(response.content)
